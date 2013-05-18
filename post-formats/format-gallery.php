82b393810166dd53ff850b295c51ea341717b0e6
Taylor Dewey <td@tddewey.com>
2013-05-18 00:36:54 -0700

    Image and Gallery post format

---
+++ post-formats/format-gallery.php
@@ -0,0 +1,2 @@
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<?php the_post_format_gallery(); ?>
---
+++ post-formats/format-image.php
@@ -0,0 +1,5 @@
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<?php the_post_format_image(); ?>
+<div class="entry">
+	<?php the_remaining_content(); ?>
+</div>
