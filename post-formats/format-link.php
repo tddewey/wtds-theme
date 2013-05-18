4ced071cfc495065584f7aee812a5b6aa26df4a6
Taylor Dewey <td@tddewey.com>
2013-05-18 00:30:54 -0700

    Link post format

---
+++ post-formats/format-link.php
@@ -0,0 +1,11 @@
+<div class="explanation">
+	<span class="explanation-text">
+		<a href="<?php the_post_format_url(); ?>"><?php the_title(); ?></a>
+	</span>
+	<span class="explanation-hand-pointing">&#9758;</span>
+</div>
+
+
+	<div class="entry">
+	<?php the_remaining_content(); ?>
+	</div>
